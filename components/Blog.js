6a2a79db3bf653e32ffa38a9fda72f8ebc405b89
Hoang Anh <dev.anhnh@gmail.com>
2026-07-14 01:29:51 +0700

    update addBlog

--- components/Blog.js
+++ components/Blog.js
@@ -1,3 +1,4 @@
+import ReactMarkdown from 'react-markdown';
 import MarkdownEditor from 'react-markdown-editor-lite';
 import 'react-markdown-editor-lite/lib/index.css';
 import Spinner from './Spinner';
@@ -50,7 +51,74 @@ export default function Blog() {
         {/* markdown description */}
         <div className="description w-100 flex flex-col flex-left mb-2">
           <label htmlFor="description">Blog Content</label>
-          <MarkdownEditor />
+          <MarkdownEditor
+            style={{ width: '100%', height: '400px' }} // you can adjust the height as needed
+            renderHTML={(text) => (
+              <ReactMarkdown
+                components={{
+                  code: ({ node, inline, className, children, ...props }) => {
+                    // for code
+                    const match = /language-(\w+)/.exec(className || '');
+
+                    if (inline) {
+                      return <code>{children}</code>;
+                    } else if (match) {
+                      return (
+                        <div style={{ position: 'relative' }}>
+                          <pre
+                            style={{ padding: '0', borderRadius: '5px', overflowX: 'auto', whiteSpace: 'pre-wrap' }}
+                            {...props}
+                          >
+                            <code>{children}</code>
+                          </pre>
+                          <button
+                            style={{ position: 'absolute', top: '0', right: '0', zIndex: '1' }}
+                            onClick={() => navigator.clipboard.writeText(children)}
+                          >
+                            copy code
+                          </button>
+                        </div>
+                      );
+                    } else {
+                      return <code {...props}>{children}</code>;
+                    }
+                  },
+                }}
+              >
+                {text}
+              </ReactMarkdown>
+            )}
+          />
+        </div>
+
+        {/* tegs */}
+        <div className="description w-100 flex flex-col flex-left mb-2">
+          <label htmlFor="tags">Tags</label>
+          <select name="tags" id="tags" multiple>
+            <option value="html">html</option>
+            <option value="javascript">javascript</option>
+            <option value="css">css</option>
+            <option value="nextjs">nextjs</option>
+            <option value="reactjs">reactjs</option>
+            <option value="vuejs">vuejs</option>
+            <option value="database">database</option>
+          </select>
+        </div>
+
+        {/* blog status */}
+        <div className="description w-100 flex flex-col flex-left mb-2">
+          <label htmlFor="status">Status</label>
+          <select name="status" id="status">
+            <option value="">No Select</option>
+            <option value="draft">Draft</option>
+            <option value="publish">Publish</option>
+          </select>
+        </div>
+
+        <div className="w-100 mb-1">
+          <button type='submit' className='w-100 addwebbtn flex-center'>
+            Save Blog
+          </button>
         </div>
       </form>
     </>
